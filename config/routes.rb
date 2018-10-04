79fc81ab0afb69905de3b1b4357f04e25d019911
Lali Gabelaya <laligabelaya@gmail.com>
2018-10-03 21:41:16 -0700

    user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'pages#index'
 
+  root 'pages#index'
   get '/help', to: 'pages#help'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
 end
