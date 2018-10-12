37b14ef77ed18fbd5c66632bea3668b13a8a22b3
Lali Gabelaya <laligabelaya@gmail.com>
2018-10-11 22:37:20 -0700

    log in method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # do something
+      log_in user
+      redirect_to root_path
+      flash[:primary] = "You are now logged in."
     else
       flash.now[:danger] = 'We do not recognize your sign-in information. Please try again.'
     render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+  # log in given user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # returns current logged in user, if there is any
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
