e61d6cc3764b940956164d256c190a6a3bca161d
Lali Gabelaya <laligabelaya@gmail.com>
2018-10-15 16:04:10 -0700

    remember_token and digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
 
   validates :first_name, presence: true, length: { minimum: 2, maximum: 30 }
@@ -11,5 +13,22 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # returns random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # returns digest token of given string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # remember user token in the DB for persistent session
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 
 end
