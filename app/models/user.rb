ea2ceadb7b7558e73f22395c4c1e3d5817e2e836
Lali Gabelaya <laligabelaya@gmail.com>
2018-10-16 18:29:56 -0700

    update action

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
     uniqueness: { case_sensitive: false }
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true 
 
   # returns a random token
   def User.new_token
